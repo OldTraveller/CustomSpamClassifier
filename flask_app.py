282af6938edbc9cd24cd4265ddbdd846a18fec37
OldTraveller <abhishekrai19999@gmail.com>
2020-07-26 20:19:09 +0530

    Spam Classifier as a Backup after the Plino Website is revoked from its hosted site

---
+++ flask_app.py
@@ -0,0 +1,66 @@
+from flask import Flask
+from flask import request as abhishek_request
+from flask import jsonify
+from naive_bayes_classifier import NaiveBayesClassifier
+import urllib.request
+import json
+import glob
+import re
+import random
+from collections import Counter
+
+PATH = "./dataset/"
+app = Flask(__name__)
+check_var = 0 
+classifier = "" 
+
+def split_data(data, prob):
+    random.shuffle(data)
+    data_len = len(data)
+    split = int(prob * data_len)
+    train_data = data[:split]
+    test_data = data[split:]
+    return train_data, test_data
+
+@app.route('/checkSpamFilter', methods=['POST'])
+def spamFilterChecker():
+      print ('Received the JAVA Request!')
+      # Get the text data from the JAVA Program.
+      req_data = abhishek_request.get_json() 
+      text_to_be_classified = req_data['text_to_be_classified']
+      print (text_to_be_classified) 
+
+      # ----------------------------------------------------------------------------
+      # Make a POST request to the plino Spam API. 
+      # ----------------------------------------------------------------------------
+      global check_var 
+      global classifier 
+      if check_var == 0: 
+            data = []
+            for verdict in ['spam', 'not_spam']:
+                  for files in glob.glob(PATH + verdict + "/*")[:500]:
+                        is_spam = True if verdict == 'spam' else False
+                        with open(files, "r", encoding='utf-8', errors='ignore') as f:
+                              for line in f:
+                                    if line.startswith("Subject:"):
+                                          subject = re.sub("^Subject: ", "", line).strip()
+                                          data.append((subject, is_spam))
+
+            random.seed(0)
+            train_data, test_data = split_data(data, 0.75)
+            classifier = NaiveBayesClassifier()
+            classifier.train(train_data)
+            check_var = 1 
+      
+      json_response = ""
+      if classifier.classify(text_to_be_classified) > 0.5: 
+            json_response = "{'email_class' : 'spam'}"
+      else:
+            json_response = "{'email_class' : 'ham'}"
+      print ("Sending Response : ") 
+      print(json_response) 
+      return json_response 
+        
+
+if __name__ =="__main__":
+    app.run(debug=True)
